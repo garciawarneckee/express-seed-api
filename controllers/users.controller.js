dbbb73b2e89aa523a0dc0fc4df991507e0e52a4b
Esteban <garciawarneckee@gmail.com>
2017-01-08 20:28:51 -0300

    added first mongoose connection

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,17 +1,31 @@
 var express = require("express");
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/seed-api")
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.json(
-        [
-            {"users": "Esteban",
-             "lastName": "Garcia"
-            },
-            {"users": "Florencia",
-             "lastName": "Prandi"
-            }
-        ]
-    )
+
+    var usersSchema = mongoose.Schema({
+        name: String,
+        lastName: String
+    });    
+    var userRepository = mongoose.model('users', usersSchema);
+
+    
+    userRepository.find(function(error, users){
+         if (error) return console.error(err);
+         res.json(users);
+    });
+    // res.json(
+    //     [
+    //         {"users": "Esteban",
+    //          "lastName": "Garcia"
+    //         },
+    //         {"users": "Florencia",
+    //          "lastName": "Prandi"
+    //         }
+    //     ]
+    // )
 })
 
 module.exports = router;
